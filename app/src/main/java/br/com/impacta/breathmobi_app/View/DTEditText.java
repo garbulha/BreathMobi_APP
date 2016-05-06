467fbcd1fe39b28c5e34c6b31ef9bfc815b92dd7
garbulha <thiago_garbulha@hotmail.com>
2016-05-06 15:21:41 -0300

    commit data

--- app/src/main/java/br/com/impacta/breathmobi_app/View/DTEditText.java
+++ app/src/main/java/br/com/impacta/breathmobi_app/View/DTEditText.java
@@ -1,7 +1,13 @@
 package br.com.impacta.breathmobi_app.View;
 
+import android.view.View;
+
 /**
  * Created by TGarbulha on 06/05/2016.
  */
-public class DTEditText {
+public class DTEditText implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+        
+    }
 }
